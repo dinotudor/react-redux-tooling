09d13755b26861cc7f5f0a7a9b45394c974d265c
Dino Tudor <dinotudor@gmail.com>
2022-03-06 18:56:59 +0100

    dogs api fetch and display

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { useAppDispatch, useAppSelector } from './app/hooks'
 import { incremented, amountAdded, decrementOne, amountDecremented, reset } from './feature/counter/counterSlice'
+import { useFetchBreedsQuery } from './feature/dogs/dogsApiSlice'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
+
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
+
+  const { data = [], isFetching } = useFetchBreedsQuery();
 
   const oneClick = () => {
     dispatch(incremented());
@@ -32,24 +36,50 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>React Redux Modern Tooling</p>
-        <p>
-          <button onClick={oneClick}>
-            Add 1
-          </button>
-          <button onClick={addMany}>
-            Add 10
-          </button>
-          <button onClick={decrement}>
-            Decrement 1
-          </button>
-          <button onClick={decrementMany}>
-            Decrement 10
-          </button>
-          <button onClick={resetCount}>
-            Reset
-          </button>
-        </p>
-        <p>count is: {count}</p>
+        <section>
+          <h1>Counter</h1>
+          <p>
+            <button onClick={oneClick}>
+              Add 1
+            </button>
+            <button onClick={addMany}>
+              Add 10
+            </button>
+            <button onClick={decrement}>
+              Decrement 1
+            </button>
+            <button onClick={decrementMany}>
+              Decrement 10
+            </button>
+            <button onClick={resetCount}>
+              Reset
+            </button>
+          </p>
+          <p>count is: {count}</p>
+        </section>
+        <section>
+          <h1>Fetch Dogos</h1>
+          Number of dogs fetched: {data.length}
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Picture</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((breed) =>{
+                return (
+                  <tr key={breed.id}>
+                    <td>{breed.name}</td>
+                    <td>
+                      <img src={breed.image.url} alt={breed.name} width={250}/></td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </section>
       </header>
     </div>
   )
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
 import counterReducer from '../feature/counter/counterSlice';
+import { apiSlice } from '../feature/dogs/dogsApiSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(apiSlice.middleware);
   }
 });
 
